2bd21840d6106a6be136c57ff629c6ac2b15d90f
Alaa Elsanteel <78312680+alaaelsanteel@users.noreply.github.com>
2023-07-27 15:12:37 +0300

    Update Index.cshtml.cs

--- ContactDatabase/ContactDatabase/ContactDatabase/Pages/Index.cshtml.cs
+++ ContactDatabase/ContactDatabase/ContactDatabase/Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using EdgeDB;
 
@@ -7,7 +7,7 @@ namespace ContactDatabase.Pages;
 public class IndexModel : PageModel
 {
     private readonly EdgeDBClient _edgeDBClient;
-    public List<Contact>Contacts { set; get; }
+    public List<Contact>Contacts { set; get; } = new List<Contact>();
     public IndexModel(EdgeDBClient edgeDBClient)
     {
         _edgeDBClient = edgeDBClient;
@@ -17,36 +17,32 @@ public class IndexModel : PageModel
         Contacts = await GetContactAsync();
     }
 
-    public async Task<IActionResult> OnPostAsync()
+    public async Task<IActionResult> OnPostAsync(Contact contact)
     {
-        var contact = new Contact
+        if (ModelState.IsValid)
         {
-            FirstName = Request.Form["FirstName"],
-            LastName = Request.Form["LastName"],
-            Email = Request.Form["Email"],
-            Title = Request.Form["Title"],
-            Description = Request.Form["Description"],
-            DateOfBirth = DateTime.Parse(Request.Form["DateOfBirth"]),
-            IsMarried = Request.Form.ContainsKey("MarriageStatus"),
+            await _edgeDBClient.ExecuteAsync(
+                "INSERT Contact{ first_name := <str>$firstName, last_name := <str>$lastName, " +
+                "email := <str>$email, title := <str>$title, description := <str>$description," +
+                "date_of_birth := <datetime>$dateOfBirth, marriage_status := <bool>$marriageStatus }",
+                new Dictionary<string, object>
+                {
+                {"firstName", contact.FirstName },
+                {"lastName", contact.LastName },
+                {"email", contact.Email},
+                {"title", contact.Title },
+                {"description", contact.Description },
+                {"dateOfBirth", contact.DateOfBirth },
+                {"marriageStatus", contact.IsMarried },
+                });
 
-        };
-        await _edgeDBClient.ExecuteAsync("INSERT Contact{ first_name := <str>$firstName, last_name := <str>$lastName, " +
-            "email := <str>$email, title := <str>$title, description := <str>$description," +
-            "date_of_birth := <datetime>$dateOfBirth, marriage_status := <bool>$marriageStatus }",
-            new Dictionary<string, object>
-            {
-                {"firstName",contact.FirstName },
-                {"lastName",contact.LastName },
-                {"email",contact.Email},
-                {"title",contact.Title },
-                {"description",contact.Description },
-                {"dateOfBirth",contact.DateOfBirth },
-                {"marriageStatus",contact.IsMarried },
-                
-            });
-        return RedirectToPage();
-        
+            return RedirectToPage();
+        }
+
+        Contacts = await GetContactAsync();
+        return Page();
     }
+
     public async Task<List<Contact>> GetContactAsync()
     {
         var result = await _edgeDBClient.QueryAsync<Contact>("SELECT Contact {FirstName :=.first_name, LastName :=.last_name, " +
@@ -57,11 +53,19 @@ public class IndexModel : PageModel
 }
 public class Contact
 {
+    [BindProperty]
     public string FirstName { get; set; } = string.Empty;
+    [BindProperty]
     public string LastName { get; set; } = string.Empty;
+    [BindProperty]
     public string Email { get; set; } = string.Empty;
+    [BindProperty]
     public string Title { get; set; } = string.Empty;
+    [BindProperty]
     public string? Description { get; set; }
-    public DateTime DateOfBirth { get; set; } 
+    [BindProperty]
+    public DateTime DateOfBirth { get; set; }
+    [BindProperty]
     public bool IsMarried { get; set; }
 }
+
